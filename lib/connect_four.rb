1caf61710d018e633e3ed3fc86e9614d21d2b72c
HMichaut <hectormichaut@gmail.com>
2021-05-29 23:53:44 +0200

    First working program

--- lib/connect_four.rb
+++ lib/connect_four.rb
@@ -1,10 +1,12 @@
 # frozen_string_literal: true
 
+require 'matrix'
+
 class ConnectFourGame
   attr_accessor :board, :player_ordered_list ### to be checked at the end
 
   def initialize
-    @heigth = 6
+    @heigth = 7
     @width = 7
     @board = Array.new(@heigth) {Array.new(@width, nil)} #Array.new(@heigth, Array.new(@width))  
     @player_ordered_list = nil
@@ -37,33 +39,90 @@ class ConnectFourGame
   end
 
   def acquire_player_choice
-    output = nil
+    output = [-1, -1]
     until valid_input?(output)
       puts 'Input x coordinate:'
-      x_value = gets.chomp.to_i
-      puts 'Input y coordinate:'
-      y_value = gets.chomp.to_i
-      output = [x_value, y_value]
+      output[0] = gets.chomp.to_i
+      output[1] = find_y_coordinate(output[0])
+      p output
     end
-    output 
+    output
+  end
+
+  def find_y_coordinate(posn_x)
+    @board.each_with_index do |value, index|
+      return index if value[posn_x].nil?
+    end
   end
 
   def valid_input?(posn)
     x = posn[0]
     y = posn[1]
-    x.between?(0, @width) && x.between?(0, @heigth) && @board[y][x] == nil
+    x.between?(0, @width) && y.between?(0, @heigth) && @board[y][x].nil?
   end
 
   def player_won?
+    token_checked = @player_ordered_list[0].token
+    token_sequence = Array.new(4, token_checked)
+    tokens_aligned_horizontal?(@board, token_sequence) || tokens_aligned_vertical?(@board, token_sequence) || tokens_aligned_diagonal?(@board, token_sequence)
+  end
+
+  def tokens_aligned_horizontal?(matrix, token_sequence)
+    matrix.any? { |item| item.each_cons(token_sequence.size).include?(token_sequence) }
+  end
+
+  def tokens_aligned_vertical?(matrix, token_sequence)
+    transposed_matrix = matrix.dup.transpose
+    tokens_aligned_horizontal?(transposed_matrix, token_sequence)
+  end
+
+  def diagonals(matrix)
+    matrix_proc = (0..matrix.size-4).map do |i|
+      (0..matrix.size-1-i).map { |j| matrix[i+j][j] }
+    end
+    matrix_proc.concat((1..matrix.first.size-4).map do |j|
+      (0..matrix.size-j-1).map { |i| matrix[i][j+i] }
+    end)
+  end
+
+  def rotate90(matrix)
+    ncols = matrix.first.size
+    matrix.each_index.with_object([]) do |i,a|
+      a << ncols.times.map { |j| matrix[j][ncols-1-i] }
+    end
+  end
+
+  def tokens_aligned_diagonal?(matrix, token_sequence)
+    diagonal_matrix = diagonals(matrix)
+    diagonal_matrix_opposed = diagonals(rotate90(matrix))
+    tokens_aligned_horizontal?(diagonal_matrix, token_sequence) || tokens_aligned_horizontal?(diagonal_matrix_opposed, token_sequence)
   end
 
   def switch_player
+    @player_ordered_list.rotate!
   end
 
   def print_board
+    # width = @board.flatten.max.to_s.size+2
+    # puts(@board.transpose.reverse.map { |a| a.map { |i| i.to_s.rjust(width) }.join })
+    @board.reverse.each { |r| p r }
   end
 
   def play
+    create_player_array
+    switch_player
+    print_board
+    until player_won? || check_free_positions.empty?
+      switch_player
+      player_choice = acquire_player_choice
+      place_token(player_choice)
+      print_board
+    end
+    if player_won?
+      puts "#{@player_ordered_list[0].name} has won the game!"
+    else
+      puts 'GAME OVER!'
+    end
   end
 end
 
@@ -76,5 +135,4 @@ class Player
   end
 end
 
-new_game = ConnectFourGame.new
-p new_game.board
+ConnectFourGame.new.play
--- spec/connect_four_spec.rb
+++ spec/connect_four_spec.rb
@@ -43,7 +43,7 @@ describe ConnectFourGame do
 
   describe '#check_free_positions' do
     it 'Returns the full array of positions if no play has been made' do
-      solution = new_game.check_free_positions - (0..6).to_a.product((0..5).to_a)
+      solution = new_game.check_free_positions - (0..6).to_a.product((0..6).to_a)
       expect(solution).to eq([])
     end
     it 'Returns a partial array if a few plays have been made' do
@@ -52,12 +52,14 @@ describe ConnectFourGame do
                         ['X', 'X', nil, 'X', 'X', 'X', 'X'],
                         ['X', 'X', 'X', nil, 'X', 'X', 'X'],
                         ['X', 'X', 'X', 'X', nil, 'X', 'X'],
-                        ['X', 'X', 'X', 'X', 'X', nil, 'X']]
+                        ['X', 'X', 'X', 'X', 'X', nil, 'X'],
+                        ['X', 'X', 'X', 'X', 'X', 'X', nil]]
       solution = new_game.check_free_positions
-      expect(solution).to eq([[0, 0], [1, 1], [2, 2], [3, 3], [4, 4], [5, 5]])
+      expect(solution).to eq([[0, 0], [1, 1], [2, 2], [3, 3], [4, 4], [5, 5], [6, 6]])
     end
     it 'Returns nil the array is full' do
       new_game.board = [['X', 'X', 'X', 'X', 'X', 'X', 'X'],
+                        ['X', 'X', 'X', 'X', 'X', 'X', 'X'],
                         ['X', 'X', 'X', 'X', 'X', 'X', 'X'],
                         ['X', 'X', 'X', 'X', 'X', 'X', 'X'],
                         ['X', 'X', 'X', 'X', 'X', 'X', 'X'],
@@ -78,14 +80,46 @@ describe ConnectFourGame do
                               [nil, nil, nil, nil, nil, nil, nil],
                               [nil, nil, nil, nil, nil, nil, nil],
                               [nil, nil, nil, nil, nil, nil, nil],
+                              [nil, nil, nil, nil, nil, nil, nil],
                               [nil, nil, nil, nil, nil, nil, nil]])
     end
   end
 
   describe '#acquire_player_choice' do
-    xit 'Check if gets receive a signal' do
-      expect(new_game).to receive(:gets)
+    before do
+      allow(new_game).to receive(:valid_input?).and_return(true)
+      # valid_input = '3'
+      # allow(new_game).to receive(:gets).and_return(valid_input)
+      # # allow(new_game).to receive(:gets).and_return(valid_input)
+    end
+    it 'Check if #valid_input? receive a signal' do
+      expect(new_game).to receive(:valid_input?)
       new_game.acquire_player_choice
+    end
+  end
+
+  describe '#find_y_coordinate' do
+    it 'Returns 0 in case of empty array.' do
+      new_game.board = [[nil, nil, nil, nil, nil, nil, nil],
+                        [nil, nil, nil, nil, nil, nil, nil],
+                        [nil, nil, nil, nil, nil, nil, nil],
+                        [nil, nil, nil, nil, nil, nil, nil],
+                        [nil, nil, nil, nil, nil, nil, nil],
+                        [nil, nil, nil, nil, nil, nil, nil],
+                        [nil, nil, nil, nil, nil, nil, nil]]
+      solution = new_game.find_y_coordinate(5)
+      expect(solution).to eql(0)
+    end
+    it 'Returns correct value in case of filled array.' do
+      new_game.board = [['X', 'X', 'X', 'X', 'X', 'X', nil],
+                        ['X', 'X', 'X', 'X', 'X', nil, nil],
+                        ['X', 'X', 'X', 'X', nil, nil, nil],
+                        ['X', 'X', 'X', 'X', nil, nil, nil],
+                        ['X', 'X', nil, nil, nil, nil, nil],
+                        ['X', nil, nil, nil, nil, nil, nil],
+                        [nil, nil, nil, nil, nil, nil, nil]]
+      solution = new_game.find_y_coordinate(3)
+      expect(solution).to eql(4)
     end
   end
 
@@ -96,8 +130,20 @@ describe ConnectFourGame do
                         ['X', 'X', nil, 'X', 'X', 'X', 'X'],
                         ['X', 'X', 'X', nil, 'X', 'X', 'X'],
                         ['X', 'X', 'X', 'X', nil, 'X', 'X'],
-                        ['X', 'X', 'X', 'X', 'X', nil, 'X']]
+                        ['X', 'X', 'X', 'X', 'X', nil, 'X'],
+                        ['X', 'X', 'X', 'X', 'X', 'X', nil]]
       solution = new_game.valid_input?([1, 1])
+      expect(solution).to be true
+    end
+    it 'Check if a valid value with on empty board returns true' do
+      new_game.board = [[nil, nil, nil, nil, nil, nil, nil],
+                        [nil, nil, nil, nil, nil, nil, nil],
+                        [nil, nil, nil, nil, nil, nil, nil],
+                        [nil, nil, nil, nil, nil, nil, nil],
+                        [nil, nil, nil, nil, nil, nil, nil],
+                        [nil, nil, nil, nil, nil, nil, nil],
+                        [nil, nil, nil, nil, nil, nil, nil]]
+      solution = new_game.valid_input?([3, 3])
       expect(solution).to be true
     end
     it 'Check if an invalid value with on empty point returns false' do
@@ -106,9 +152,10 @@ describe ConnectFourGame do
                         ['X', 'X', nil, 'X', 'X', 'X', 'X'],
                         ['X', 'X', 'X', nil, 'X', 'X', 'X'],
                         ['X', 'X', 'X', 'X', nil, 'X', 'X'],
-                        ['X', 'X', 'X', 'X', 'X', nil, 'X']]
+                        ['X', 'X', 'X', 'X', 'X', nil, 'X'],
+                        ['X', 'X', 'X', 'X', 'X', 'X', nil]]
       solution = new_game.valid_input?([10, 10])
-      expect(solution).to_not be true
+      expect(solution).to be false
     end
     it 'Check if a valid value with on full point returns false' do
       new_game.board = [[nil, 'X', 'X', 'X', 'X', 'X', 'X'],
@@ -116,65 +163,207 @@ describe ConnectFourGame do
                         ['X', 'X', nil, 'X', 'X', 'X', 'X'],
                         ['X', 'X', 'X', nil, 'X', 'X', 'X'],
                         ['X', 'X', 'X', 'X', nil, 'X', 'X'],
-                        ['X', 'X', 'X', 'X', 'X', nil, 'X']]
+                        ['X', 'X', 'X', 'X', 'X', nil, 'X'],
+                        ['X', 'X', 'X', 'X', 'X', 'X', nil]]
       solution = new_game.valid_input?([1, 0])
-      expect(solution).to_not be true
+      expect(solution).to be false
+    end
+  end
+
+  describe '#tokens_aligned_horizontal?' do
+    it 'Returns true for an horizontal line' do
+      token_sequence = Array.new(4, 'O')
+      input_array = [[nil, 'O', 'O', 'O', 'O', nil, nil],
+                     [nil, nil, nil, nil, nil, nil, nil],
+                     [nil, nil, nil, nil, nil, nil, nil],
+                     [nil, nil, nil, nil, nil, nil, nil],
+                     [nil, nil, nil, nil, nil, nil, nil],
+                     [nil, nil, nil, nil, nil, nil, nil],
+                     [nil, nil, nil, nil, nil, nil, nil]]
+      solution = new_game.tokens_aligned_horizontal?(input_array, token_sequence)
+      expect(solution).to be true
+    end
+    it 'Returns false for an broken horizontal line' do
+      token_sequence = Array.new(4, 'O')
+      input_array = [[nil, 'O', 'O', 'O', nil, 'O', nil],
+                     [nil, nil, nil, nil, nil, nil, nil],
+                     [nil, nil, nil, nil, nil, nil, nil],
+                     [nil, nil, nil, nil, nil, nil, nil],
+                     [nil, nil, nil, nil, nil, nil, nil],
+                     [nil, nil, nil, nil, nil, nil, nil],
+                     [nil, nil, nil, nil, nil, nil, nil]]
+      solution = new_game.tokens_aligned_horizontal?(input_array, token_sequence)
+      expect(solution).to be false
+    end
+    it 'Returns false for a horizontal line broken by wrong token' do
+      token_sequence = Array.new(4, 'O')
+      input_array = [[nil, 'O', 'O', 'O', 'X', 'O', nil],
+                     [nil, nil, nil, nil, nil, nil, nil],
+                     [nil, nil, nil, nil, nil, nil, nil],
+                     [nil, nil, nil, nil, nil, nil, nil],
+                     [nil, nil, nil, nil, nil, nil, nil],
+                     [nil, nil, nil, nil, nil, nil, nil],
+                     [nil, nil, nil, nil, nil, nil, nil]]
+      solution = new_game.tokens_aligned_horizontal?(input_array, token_sequence)
+      expect(solution).to be false
+    end
+  end
+
+  describe '#tokens_aligned_vertical?' do
+    it 'Returns true for an vertical line' do
+      token_sequence = Array.new(4, 'O')
+      input_array = [[nil, 'O', nil, nil, nil, nil, nil],
+                     [nil, 'O', nil, nil, nil, nil, nil],
+                     [nil, 'O', nil, nil, nil, nil, nil],
+                     [nil, 'O', nil, nil, nil, nil, nil],
+                     [nil, nil, nil, nil, nil, nil, nil],
+                     [nil, nil, nil, nil, nil, nil, nil],
+                     [nil, nil, nil, nil, nil, nil, nil]]
+      solution = new_game.tokens_aligned_vertical?(input_array, token_sequence)
+      expect(solution).to be true
+    end
+    it 'Returns false for an broken vertical line' do
+      token_sequence = Array.new(4, 'O')
+      input_array = [[nil, 'O', nil, nil, nil, 'O', nil],
+                     [nil, 'O', nil, nil, nil, nil, nil],
+                     [nil, 'O', nil, nil, nil, nil, nil],
+                     [nil, nil, nil, nil, nil, nil, nil],
+                     [nil, 'O', nil, nil, nil, nil, nil],
+                     [nil, nil, nil, nil, nil, nil, nil],
+                     [nil, nil, nil, nil, nil, nil, nil]]
+      solution = new_game.tokens_aligned_vertical?(input_array, token_sequence)
+      expect(solution).to be false
+    end
+    it 'Returns false for a vertical line broken by wrong token' do
+      token_sequence = Array.new(4, 'O')
+      input_array = [[nil, 'O', nil, 'O', 'X', 'O', nil],
+                     [nil, 'O', nil, nil, nil, nil, nil],
+                     [nil, 'O', nil, nil, nil, nil, nil],
+                     [nil, 'X', nil, nil, nil, nil, nil],
+                     [nil, 'O', nil, nil, nil, nil, nil],
+                     [nil, nil, nil, nil, nil, nil, nil],
+                     [nil, nil, nil, nil, nil, nil, nil]]
+      solution = new_game.tokens_aligned_vertical?(input_array, token_sequence)
+      expect(solution).to be false
+    end
+  end
+
+  describe '#tokens_aligned_diagonal?' do
+    it 'Returns true for a diagonal line' do
+      token_sequence = Array.new(4, 'X')
+      input_array = [['X', nil, nil, nil, nil, nil, nil],
+                     ['X', nil, nil, nil, nil, nil, nil],
+                     [nil, 'X', nil, nil, nil, nil, nil],
+                     [nil, nil, 'X', nil, nil, nil, nil],
+                     [nil, nil, nil, 'X', nil, nil, nil],
+                     [nil, nil, nil, nil, nil, nil, nil],
+                     [nil, nil, nil, nil, nil, nil, nil]]
+      solution = new_game.tokens_aligned_diagonal?(input_array, token_sequence)
+      expect(solution).to be true
+    end
+    it 'Returns true for a diagonal line on the other side' do
+      token_sequence = Array.new(4, 'X')
+      input_array = [[nil, nil, nil, nil, nil, nil, 'X'],
+                     [nil, nil, nil, nil, nil, 'X', nil],
+                     [nil, nil, nil, nil, 'X', nil, nil],
+                     [nil, nil, nil, 'X', nil, nil, nil],
+                     [nil, nil, nil, nil, nil, nil, nil],
+                     [nil, nil, nil, nil, nil, nil, nil],
+                     [nil, nil, nil, nil, nil, nil, nil]]
+      solution = new_game.tokens_aligned_diagonal?(input_array, token_sequence)
+      expect(solution).to be true
+    end
+    it 'Returns false for a vertical line broken by wrong token' do
+      token_sequence = Array.new(4, 'X')
+      input_array = [[nil, nil, nil, nil, nil, nil, 'X'],
+                     [nil, nil, nil, nil, nil, 'X', nil],
+                     [nil, nil, nil, nil, 'O', nil, nil],
+                     [nil, nil, nil, 'X', nil, nil, nil],
+                     [nil, nil, 'X', nil, nil, nil, nil],
+                     [nil, nil, nil, nil, nil, nil, nil],
+                     [nil, nil, nil, nil, nil, nil, nil]]
+      solution = new_game.tokens_aligned_diagonal?(input_array, token_sequence)
+      expect(solution).to be false
+    end
+    it 'Returns false for a vertical line broken' do
+      token_sequence = Array.new(4, 'X')
+      input_array = [[nil, nil, nil, nil, nil, nil, 'X'],
+                     [nil, nil, nil, nil, nil, 'X', nil],
+                     [nil, nil, nil, nil, nil, nil, nil],
+                     [nil, nil, nil, 'X', nil, nil, nil],
+                     [nil, nil, 'X', nil, nil, nil, nil],
+                     [nil, nil, nil, nil, nil, nil, nil],
+                     [nil, nil, nil, nil, nil, nil, nil]]
+      solution = new_game.tokens_aligned_diagonal?(input_array, token_sequence)
+      expect(solution).to be false
     end
   end
 
   describe '#player_won?' do
-    xit 'Returns true for an horizontal line' do
-      new_game.board = [[nil, 'X', 'X', 'X', 'X', nil, nil],
+    it 'Returns true for an horizontal line' do
+      new_game.create_player_array
+      new_game.board = [[nil, 'O', 'O', 'O', 'O', nil, nil],
                         [nil, nil, nil, nil, nil, nil, nil],
                         [nil, nil, nil, nil, nil, nil, nil],
+                        [nil, nil, nil, nil, nil, nil, nil],
+                        [nil, nil, nil, nil, nil, nil, nil],
+                        [nil, nil, nil, nil, nil, nil, nil],
+                        [nil, nil, nil, nil, nil, nil, nil]]
+      solution = new_game.player_won?
+      expect(solution).to be true
+    end
+    it 'Returns true for an vertical line line' do
+      new_game.create_player_array
+      new_game.board = [['O', nil, nil, nil, nil, nil, nil],
+                        ['O', nil, nil, nil, nil, nil, nil],
+                        ['O', nil, nil, nil, nil, nil, nil],
+                        ['O', nil, nil, nil, nil, nil, nil],
                         [nil, nil, nil, nil, nil, nil, nil],
                         [nil, nil, nil, nil, nil, nil, nil],
                         [nil, nil, nil, nil, nil, nil, nil]]
       solution = new_game.player_won?
       expect(solution).to be true
     end
-    xit 'Returns true for an vertical line line' do
-      new_game.board = [['X', nil, nil, nil, nil, nil, nil],
-                        ['X', nil, nil, nil, nil, nil, nil],
-                        ['X', nil, nil, nil, nil, nil, nil],
-                        ['X', nil, nil, nil, nil, nil, nil],
+    it 'Returns true for a diagonal line' do
+      new_game.create_player_array
+      new_game.board = [['O', nil, nil, nil, nil, nil, nil],
+                        [nil, 'O', nil, nil, nil, nil, nil],
+                        [nil, nil, 'O', nil, nil, nil, nil],
+                        [nil, nil, nil, 'O', nil, nil, nil],
+                        [nil, nil, nil, nil, nil, nil, nil],
                         [nil, nil, nil, nil, nil, nil, nil],
                         [nil, nil, nil, nil, nil, nil, nil]]
       solution = new_game.player_won?
       expect(solution).to be true
     end
-    xit 'Returns true for a diagonal line' do
-      new_game.board = [['X', nil, nil, nil, nil, nil, nil],
-                        [nil, 'X', nil, nil, nil, nil, nil],
-                        [nil, nil, 'X', nil, nil, nil, nil],
-                        [nil, nil, nil, 'X', nil, nil, nil],
+    it 'Returns true for a diagonal line on the other side' do
+      new_game.create_player_array
+      new_game.board = [[nil, nil, nil, nil, nil, nil, 'O'],
+                        [nil, nil, nil, nil, nil, 'O', nil],
+                        [nil, nil, nil, nil, 'O', nil, nil],
+                        [nil, nil, nil, 'O', nil, nil, nil],
+                        [nil, nil, nil, nil, nil, nil, nil],
                         [nil, nil, nil, nil, nil, nil, nil],
                         [nil, nil, nil, nil, nil, nil, nil]]
       solution = new_game.player_won?
       expect(solution).to be true
     end
-    xit 'Returns true for a diagonal line on the other side' do
-      new_game.board = [[nil, nil, nil, nil, nil, nil, 'X'],
-                        [nil, nil, nil, nil, nil, 'X', nil],
-                        [nil, nil, nil, nil, 'X', nil, nil],
-                        [nil, nil, nil, 'X', nil, nil, nil],
-                        [nil, nil, nil, nil, nil, nil, nil],
-                        [nil, nil, nil, nil, nil, nil, nil]]
-      solution = new_game.player_won?
-      expect(solution).to be true
-    end
-    xit 'Returns false for an empty board' do
+    it 'Returns false for an empty board' do
+      new_game.create_player_array
       new_game.board = [[nil, nil, nil, nil, nil, nil, nil],
                         [nil, nil, nil, nil, nil, nil, nil],
                         [nil, nil, nil, nil, nil, nil, nil],
                         [nil, nil, nil, nil, nil, nil, nil],
                         [nil, nil, nil, nil, nil, nil, nil],
+                        [nil, nil, nil, nil, nil, nil, nil],
                         [nil, nil, nil, nil, nil, nil, nil]]
       solution = new_game.player_won?
       expect(solution).to_not be true
     end
-    xit 'Returns false for a line with different tokens' do
-      new_game.board = [[nil, nil, nil, 'X', 'X', 'O', 'X'],
+    it 'Returns false for a line with different tokens' do
+      new_game.create_player_array
+      new_game.board = [[nil, nil, nil, 'O', 'O', 'O', 'X'],
+                        [nil, nil, nil, nil, nil, nil, nil],
                         [nil, nil, nil, nil, nil, nil, nil],
                         [nil, nil, nil, nil, nil, nil, nil],
                         [nil, nil, nil, nil, nil, nil, nil],
@@ -186,13 +375,13 @@ describe ConnectFourGame do
   end
 
   describe '#switch_player' do
-    xit 'switch from player 1 to player 2' do
+    it 'switch from player 1 to player 2' do
       new_game.player_ordered_list = [player_1, player_2]
       new_game.switch_player
       solution = new_game.player_ordered_list
       expect(solution).to eq([player_2, player_1])
     end
-    xit 'switch from player 2 to player 1' do
+    it 'switch from player 2 to player 1' do
       new_game.player_ordered_list = [player_2, player_1]
       new_game.switch_player
       solution = new_game.player_ordered_list
